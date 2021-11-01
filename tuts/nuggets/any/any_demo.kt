6f94f62e78b240583c81d3524b05fd65fda8555f
Christoph Pickl <c.pickl@tbauctions.com>
2021-11-01 13:32:04 +0100

    misc

--- tuts/nuggets/any/any_demo.kt
+++ tuts/nuggets/any/any_demo.kt
@@ -21,25 +21,19 @@ class SomeAuctionTest : StringSpec() {
         "is old enough - too verbose" {
             // title defined here is irelevant, should be hidden! :-/
             val auction = Auction("foo", 12)
-
             val result = AuctionService.isOldEnough(auction)
-
             result shouldBe false
         }
         "is old enough - coupling" {
             // this requires to have a StringSpec scope, making it not reusable in any other context/test type! :-(
             val auction = newAuction()
-
             val result = AuctionService.isOldEnough(auction)
-
             result shouldBe false
         }
         "is old enough - improved" {
             // hide the irrelevant, and only state what's relevant for this specific test case :-)
             val auction = Auction.any().copy(age = 12)
-
             val result = AuctionService.isOldEnough(auction)
-
             result shouldBe false
         }
     }
@@ -47,14 +41,12 @@ class SomeAuctionTest : StringSpec() {
 
 // IDE warns in full justification about an unused receiver parameter, as we introduce a totally unnecessary coupling here! :-(
 fun StringSpecScope.newAuction() = Auction("foo", 10)
-
 fun createAuction() = Auction("foo", 10)
 fun makeAuction() = Auction("foo", 10)
 fun buildAuction() = Auction("foo", 10)
 
 // while refactoring type name, this function stayed oldschool :-/
 fun createAuctionItem() = Auction("foo", 10)
-
 fun Auction.Companion.any() = Auction(
     title = "anyTitle",
     age = -1
@@ -69,7 +61,7 @@ fun createAuction(
     title = title,
     age = age
 )
-
+// ITUtilManagerHelperGodClass?!
 class IntegrationTestUtil(val database: Any) {
     // this guy is (unnecessarily) amazingly difficult to reach! :-/
     fun createAuction() = Auction("", 1)
